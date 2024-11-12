ff64f75609b6572f1debd448e94544bc443cac91
Den Smalonski <dzianis.smalonski@protofire.io>
2024-11-12 09:18:47 +0100

    fix: update text

--- src/components/common/CookieAndTermBanner/index.tsx
+++ src/components/common/CookieAndTermBanner/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, type ReactElement } from 'react'
 import classnames from 'classnames'
 import type { CheckboxProps } from '@mui/material'
-import { Grid, Button, Checkbox, FormControlLabel, Typography, Paper, SvgIcon, Box } from '@mui/material'
+import { Grid, Button, Checkbox, FormControlLabel, Typography, Paper, SvgIcon, Box, Link } from '@mui/material'
 import WarningIcon from '@/public/images/notifications/warning.svg'
 import { useForm } from 'react-hook-form'
 import { metadata } from '@/markdown/terms/terms.md'
@@ -17,7 +17,6 @@ import { selectCookieBanner, openCookieBanner, closeCookieBanner } from '@/store
 
 import css from './styles.module.css'
 import { AppRoutes } from '@/config/routes'
-import ExternalLink from '../ExternalLink'
 
 const COOKIE_AND_TERM_WARNING: Record<CookieAndTermType, string> = {
   [CookieAndTermType.TERMS]: '',
@@ -87,10 +86,14 @@ export const CookieAndTermBanner = ({
           <Grid item xs>
             <Typography variant="body2" mb={2}>
               By browsing this page, you accept our{' '}
-              <ExternalLink href={AppRoutes.terms}>Terms & Conditions</ExternalLink> (last updated{' '}
-              {metadata.last_update_date}) and the use of necessary cookies. By clicking &quot;Accept all&quot; you
-              additionally agree to the use of Beamer and Analytics cookies as listed below.{' '}
-              <ExternalLink href={AppRoutes.cookie}>Cookie policy</ExternalLink>
+              <Link href={AppRoutes.terms} style={{ textDecoration: 'underline' }}>
+                Terms & Conditions
+              </Link>{' '}
+              (last updated October 2024) and the use of necessary cookies.{' '}
+              <Link href={AppRoutes.cookie} style={{ textDecoration: 'underline' }}>
+                Cookie policy
+              </Link>
+              .
             </Typography>
 
             <Grid container alignItems="center" gap={4}>
